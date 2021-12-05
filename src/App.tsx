fe197de3b8644f84febaf16f76d49372321f8151
Sergiu Voicu <sergiu.ciprian.voicu@gmail.com>
2021-12-05 03:54:48 +0200

    merge fix again

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import { Redirect, Route } from "react-router-dom";
 import { IonApp, IonRouterOutlet } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import Home from "./pages/Home/Home";
-<<<<<<< HEAD
-
-=======
 import Login from "./pages/Login/Login";
+import Profile from "./pages/Profile/Profile";
 import { useSelector } from "react-redux";
->>>>>>> 6838ec6ba3e13fe28d056097476325ff393b1283
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
 
@@ -26,9 +23,9 @@ import "@ionic/react/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
-import Login from "./pages/Login/Login";
-import Profile from "./pages/Profile/Profile";
-import { useSelector } from "react-redux";
+
+
+
 
 interface StoreState {
   user: { loggedIn: boolean };
